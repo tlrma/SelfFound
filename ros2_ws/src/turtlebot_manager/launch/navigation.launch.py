b3fc31bd47b74db1a3c198c3a4eaa7f578a3b1b7
onono1141@gmail.com <onono1141@gmail.com>
2026-06-02 17:34:31 +0900

    [turtlebot] 맵, world, named pose, navigation launch 파일 추가

---
+++ ros2_ws/src/turtlebot_manager/launch/navigation.launch.py
@@ -0,0 +1,83 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory('turtlebot_manager')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    turtlebot3_gazebo = get_package_share_directory('turtlebot3_gazebo')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    world = os.path.join(pkg_dir, 'worlds', 'lost_and_found.world')
+    map_file = os.path.join(pkg_dir, 'maps', 'pjt_map.yaml')
+
+    gzserver = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items()
+    )
+
+    gzclient = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+        )
+    )
+
+    robot_state_publisher = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(turtlebot3_gazebo, 'launch', 'robot_state_publisher.launch.py')
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    spawn_turtlebot = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(turtlebot3_gazebo, 'launch', 'spawn_turtlebot3.launch.py')
+        ),
+        launch_arguments={
+            'x_pose': '-1.5',
+            'y_pose': '-1.0'
+        }.items()
+    )
+
+    nav2 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('turtlebot3_navigation2'),
+                'launch',
+                'navigation2.launch.py'
+            )
+        ),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+            'map': map_file,
+            'params_file': os.path.join(
+                get_package_share_directory('turtlebot3_navigation2'),
+                'param',
+                'waffle_pi.yaml'
+            )
+        }.items()
+    )
+
+    rviz2 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('nav2_bringup'),
+                'launch',
+                'rviz_launch.py'
+            )
+        )
+    )
+
+    return LaunchDescription([
+        gzserver,
+        gzclient,
+        robot_state_publisher,
+        spawn_turtlebot,
+        TimerAction(period=5.0, actions=[nav2]),
+        #TimerAction(period=7.0, actions=[rviz2]),
+    ])
---
+++ ros2_ws/src/turtlebot_manager/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+
+package_name = 'turtlebot_manager'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py')),
+        (os.path.join('share', package_name, 'worlds'),
+            glob('worlds/*.world')),
+        (os.path.join('share', package_name, 'maps'),
+            glob('maps/*')),
+        (os.path.join('share', package_name, 'config'),
+            glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ssafy',
+    maintainer_email='onono1141@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'navigate_to_named_pose = turtlebot_manager.navigate_to_named_pose:main',
+        ],
+    },
+)
